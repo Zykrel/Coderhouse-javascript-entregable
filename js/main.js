ce457717b0890211d31805c5547b41c3f0b8b649
nahuel <kamaelnahuel@gmail.com>
2022-11-01 16:17:28 -0300

    Modificaciones

--- js/main.js
+++ js/main.js
@@ -1,41 +1,70 @@
 //Calculadora de productos
-
-function menu(){
-    let producto = prompt('Elija su producto. \n Monitor Gamer \n Procesador AMD \n Combo de Accesorios').toLowerCase();
-    let resultado = mostrarPrecio(producto);
-    if(resultado == 'error'){
-        alert('error al ingresar');
-    }else{
-        alert('El precio es: ' + resultado );
+class Placard {
+    constructor(id, nombre, medida, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.medida = medida;
+        this.precio = precio;
     }
+}
 
-    let opcion = prompt('Desea volver a comenzar \n Si | No').toLowerCase()
+const placard1 = new Placard(1, 'placard color blanco', '120x182x45', 20000)
+const placard2 = new Placard(2, 'Placard color artico', '150x182x45', 35000)
+const placard3 = new Placard(3, 'Placard color roble', '180x182x45', 42000)
+const placard4 = new Placard(4, 'Placard color nature', '2x182x45', 47000)
+
+
+const placares = [placard1, placard2, placard3, placard4]
+
+const carrito = []
+
+
+
+function menu() {
+    let producto = prompt(`Elija su producto(seleccione el numero correspondiente)
+    ${placares.map((placard) => {
+        return '\n' + placard.id + ' ' + placard.nombre + ' $' + placard.precio
+    })}
+    `).toLowerCase();
+    producto = parseInt(producto)
+    const placardSeleccionado = placares.find(placard => placard.id === producto)
+    if (placardSeleccionado == undefined) {
+        alert('error al ingresar');
+    } else {
+        let cantidad = prompt(`Producto seleccionado: ${placardSeleccionado.nombre}. Seleccione la cantidad deseada`)
+        cantidad = parseInt(cantidad)
+        agregarAlCarrito(cantidad, placardSeleccionado)
+    }
+    let opcion = prompt('¿Desea agregar mas productos? \n Si | No').toLowerCase()
     return opcion
 }
 
-function mostrarPrecio(producto){
-    switch(producto){
-        case 'monitor gamer':
-            return '$25.000';
-            break;
-        case 'procesador amd':
-            return '$22.500';
-            break;
-        case 'combo de accesorios':
-            return '$4.500';
-            break;
-        default:
-            return 'error';
-            break;
+function agregarAlCarrito(cantidad, productoSeleccionado) {
+    for (let i = 0; i < cantidad; i++) {
+        carrito.push(productoSeleccionado);
     }
 }
 
-function inicio(){
+function mostrarCarrito() {
+
+    let total = 0
+    carrito.forEach((producto) => {
+        total = producto.precio + total
+    })
+
+    return `Carrito: 
+    ${carrito.map((placard) => {
+        return '\n' + placard.nombre + ' $' + placard.precio
+    })}
+    total: $ ${total}`
+}
+
+function inicio() {
     let opcion = ''
-    do{
-        opcion =  menu()
-    }while(opcion == 'si')
-    alert('Fin del Programa :D')
+    do {
+        opcion = menu()
+    } while (opcion == 'si')
+    alert(mostrarCarrito())
 }
 inicio()
 
